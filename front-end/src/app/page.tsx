49cceea2afbe536dcbb5c6f9d627e0e6186cd57c
김영섭 <kimyoungseop33@gmail.com>
2024-03-20 10:44:16 +0900

    Fix: Update front-end for CORS

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -7,15 +7,12 @@ export default function Home() {
   const handleClick = async () => {
     console.log("click");
     try {
-      const response = await fetch(
-        "http://localhost:8081/api/v1/auth/oauth2/kakao",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch("/api/v1/auth/oauth2/kakao", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      });
       if (!response.ok) {
         throw new Error("로그인 요청 실패");
       }
